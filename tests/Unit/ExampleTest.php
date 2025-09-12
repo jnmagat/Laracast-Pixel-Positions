ce66c072657c246168f779d4863783014c72a568
Jonathan <jonathanmagat96@gmail.com>
2025-09-12 14:37:13 +0800

    tried some model logic testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,45 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Employer;
+use App\Models\User;
+use App\Models\Job;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      */
-    public function test_that_true_is_true(): void
+    // public function test_that_true_is_true(): void
+    // {
+    //     $this->assertTrue(true);
+    // }
+
+    public function test_employer_belongs_to_user()
     {
-        $this->assertTrue(true);
+        $user = User::factory()->create();
+        $employer = Employer::factory()->create(['user_id' => $user->id]);
+
+        $this->assertTrue($employer->user->is($user));
+    }
+
+    public function test_job_belongs_to_employer()
+    {
+        $employer = Employer::factory()->create();
+        $job = Job::factory()->create(['employer_id' => $employer->id]);
+
+        $this->assertTrue($job->employer->is($employer));
+    }
+
+    public function test_can_have_tags()
+    {
+        $job = Job::factory()->create();
+        $job->tag('Frontend');
+
+        $this->assertCount(1, $job->tags);
     }
 }
